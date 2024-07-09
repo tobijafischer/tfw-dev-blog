dc1b33841bdc70fef8e822fec60c4ef6170bdc9a
Tobija Fischer <45046461+tobijafischer@users.noreply.github.com>
2024-07-09 10:24:23 +0200

    upgraded nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,24 @@
 export default defineNuxtConfig({
   ssr: true,
   devtools: { enabled: true },
+
   modules: [
     '@nuxt/content',
     '@nuxthq/studio',
     '@vueuse/nuxt',
     'nuxt-lenis',
   ],
+
   app: {
     head: {
-      link: [{ rel: 'stylesheet', href: 'https://use.typekit.net/kxl6riz.css' }]
+      link: [{ rel: 'stylesheet', href: 'https://use.typekit.net/tpz3sln.css' }]
     }
   },
+
   css: [
     '~/assets/css/reset.css',
     '~/assets/css/app.css',
   ],
+
+  compatibilityDate: '2024-07-09',
 })
